1e7e698f50eff744e4cad6115884e5d4cc008258
Valentin THOMAS <vthomas@e3r13p1.42.fr>
2016-05-05 09:53:32 +0200

    correct the tetri_new error

--- src/tetri_new.c
+++ src/tetri_new.c
@@ -6,7 +6,7 @@
 /*   By: vthomas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/05 06:41:53 by vthomas           #+#    #+#             */
-/*   Updated: 2016/05/05 09:24:38 by aviau            ###   ########.fr       */
+/*   Updated: 2016/05/05 09:51:59 by vthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_tetri	*tetri_new(t_tetri *last, size_t nb, char *tetri)
 	tetri_new = ft_memalloc(sizeof(t_tetri));
 	if (tetri_new == NULL)
 		tetri_exit(NULL);
-	if (last == NULL)
+	if (last != NULL)
 		last->next = tetri_new;
 	tetri_new->last = last;
 	tetri_new->next = NULL;
