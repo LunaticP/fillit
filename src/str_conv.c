27c397ee52dc0f73ad7f401b43d34e3b4e489d18
Axel VIAU <aviau@e3r12p2.42.fr>
2016-05-10 04:57:34 +0200

    str_conv replace position

--- src/str_conv.c
+++ src/str_conv.c
@@ -6,17 +6,55 @@
 /*   By: vthomas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/05 10:32:33 by vthomas           #+#    #+#             */
-/*   Updated: 2016/05/05 11:55:17 by vthomas          ###   ########.fr       */
+/*   Updated: 2016/05/10 04:54:34 by aviau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+char	*replace(char *input)
+{
+	int		x;
+	char	**t;
+
+	t = ft_strsplit(input, '\n');
+	ft_putendl(t[0]);
+	ft_putendl(t[1]);
+	ft_putendl(t[2]);
+	ft_putendl(t[3]);
+	ft_putchar('\n');
+	while (t[0][0] == '.' && t[1][0] == '.' && t[2][0] == '.' && t[3][0] == '.')
+	{
+		x = -1;
+		while (x++ < 3)
+		{
+			t[0][x] = (t[0][x + 1]) ? t[0][x + 1] : '.';
+			t[1][x] = (t[1][x + 1]) ? t[1][x + 1] : '.';
+			t[2][x] = (t[2][x + 1]) ? t[2][x + 1] : '.';
+			t[3][x] = (t[3][x + 1]) ? t[3][x + 1] : '.';
+		}
+	}
+	while (!ft_strcmp(t[0], "....") && (x = -1))
+		while (x++ < 3)
+		{
+			t[x] = (t[x + 1]) ? t[x + 1] : "....";
+			t[x] = (t[x + 1]) ? t[x + 1] : "....";
+			t[x] = (t[x + 1]) ? t[x + 1] : "....";
+			t[x] = (t[x + 1]) ? t[x + 1] : "....";
+		}
+	ft_putendl(t[0]);
+	ft_putendl(t[1]);
+	ft_putendl(t[2]);
+	ft_putendl(t[3]);
+	return (input);
+}
 
 char	*str_conv(char *input)
 {
 	char	*output;
 
 	output = ft_strnew(16);
+	input = replace(input);
 	if (output == NULL)
 		tetri_exit(NULL);
 	ft_strncpy(&output[0], &input[0], 4);
